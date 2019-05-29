c730d9367a228a2259846fce647543aff9cb8388
hariharans29 <shariharan91@gmail.com>
2019-05-29 13:31:25 -0700

    More changes

--- onnx-ecosystem/inference_demos/ssd_deps/predict_pb2.py
+++ onnx-ecosystem/inference_demos/ssd_deps/predict_pb2.py
@@ -12,7 +12,7 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-from ssd_deps import onnx_ml_pb2 as onnx__ml__pb2
+import onnx_ml_pb2 as onnx__ml__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
